69095ab1999fedb92dc8701a2d198b93d6d3704b
Glenn <glenn@geenentijd.be>
2015-05-22 16:44:24 +0200

    fixes

---
+++ test.js
@@ -0,0 +1,25 @@
+var serial = require('./serial');
+
+var a = 0;
+var b = 0;
+
+console.log('listPorts');
+serial.listPorts(function() {
+	console.log('list done');
+});
+
+clock();
+
+function clock() {
+	++a;
+	++b;
+
+	if(a > 33) {
+		a = 0;	
+	}	
+
+	serial.sendLines(a, b);
+	setTimeout(clock, 1000);
+}
+
+
